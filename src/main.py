5b9374385286d44d8fc5f9f1244a77b08e1d234c
Tony Travis <tony.travis12@gmail.com>
2026-03-26 21:31:57 +0000

    implemented textnode classes and tested via main

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+print('hello world')
+
+def main():
+    print(TextNode('Blah blah, anchor text, blah blah', TextType.BOLD, "https://www.boot.dev"))
+  
+
+main()
