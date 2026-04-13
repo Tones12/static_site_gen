8083d0f2d3ed4fb75383b0ccb00dacd6976fbb40
Tony Travis <tony.travis12@gmail.com>
2026-04-13 17:00:26 +0000

    full functionality complete

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,6 +3,8 @@ import shutil
 from markdown_to_html_node import markdown_to_html_node
 from extract_title import extract_title
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from pathlib import Path
+
 
 
 def generate_page(from_path, template_path, dest_path):
@@ -16,16 +18,33 @@ def generate_page(from_path, template_path, dest_path):
     template = str(template_file.read())
     template_file.close()
 
-    html_nodes = markdown_to_html_node(markdown)
-    html = html_nodes.to_html()
+    html_node = markdown_to_html_node(markdown)
+    html = html_node.to_html()
+    
     title = extract_title(markdown)
-    template.replace("{{ Title }}", title)
-    template.replace("{{ Content }}", html)
-    os.path.isdir(dest_path) or os.makedirs(dest_path)
+   
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    
+    directory = os.path.dirname(dest_path)
+    os.path.isdir(directory) or os.makedirs(directory)
+    
     #create new html file in destination path using updated template
-    html_file = open(os.path.join(dest_path, "index.html"), 'w')
+    html_file = open(dest_path, 'w')
     html_file.write(template)
     html_file.close()
     
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
+    #make list of files and folders in dir_path_content
+    dir_list = os.listdir(dir_path_content)
+    print(dir_list)
+    for item in dir_list:
+        dir_path_item = os.path.join(dir_path_content, item)
+        dest_dir_path_item = os.path.join(dest_dir_path, item)
+        if os.path.isdir(dir_path_item):
+            generate_pages_recursive(dir_path_item, template_path, dest_dir_path_item)
+        else:
+            dest_dir_path_item = Path(dest_dir_path_item).with_suffix('.html')
+            generate_page(dir_path_item, template_path, dest_dir_path_item)
     
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from textnode import TextNode, TextType
 from static_folder_to_public import static_folder_to_public_folder
-
+from generate_page import generate_pages_recursive
 
 print('Robot Voice: "Static Site Generator... Initializing..."')
 
 def main():
     static_folder_to_public_folder()
-  
+
+    generate_pages_recursive('content', 'template.html', 'public' )
 
 main()
